135159c22a0f679073caa442e6c202cb565b8ce5
Rodrigoch98 <rodrigocesarhang@gmail.com>
2025-02-03 08:10:14 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 // scripts.js
 // Adiciona interatividade ao menu mobile
 document.addEventListener('DOMContentLoaded', function () {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
 
-    navbarToggler.addEventListener('click', function () {
-        navbarCollapse.classList.toggle('show');
-    });
+  navbarToggler.addEventListener('click', function () {
+    navbarCollapse.classList.toggle('show');
+  });
 
-    // Fecha o menu ao clicar em um link
-    const navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            navbarCollapse.classList.remove('show');
-        });
+  // Fecha o menu ao clicar em um link
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navbarCollapse.classList.remove('show');
     });
+  });
 });
