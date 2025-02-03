112db32b78a6325a9d1573de02a4b7c17298c79c
Rodrigoch98 <rodrigocesarhang@gmail.com>
2025-02-03 11:46:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // scripts.js
-// Interatividade do menu mobile e atualização automática do ano (se desejado)
+// Interatividade do menu mobile e (se desejado) atualização automática do ano
 document.addEventListener('DOMContentLoaded', function () {
   const navbarToggler = document.querySelector('.navbar-toggler');
   const navbarCollapse = document.querySelector('.navbar-collapse');
